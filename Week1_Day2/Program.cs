f506a0cae201d6fc3dccee8c4406cd9f0d629f84
alexandriaware <ali.mercer92@gmail.com>
2017-09-26 14:19:20 -0400

    Practiced with data types

--- Week1_Day2/Program.cs
+++ Week1_Day2/Program.cs
@@ -10,6 +10,18 @@ namespace Week1_Day2
     {
         static void Main(string[] args)
         {
+            char firstInitial = 'D';
+
+            bool isTired = true;
+
+            //other number data types
+            float number = 2.164403304907294849383961032f;
+            decimal partialNum = 2.164403304907294849383961032m;
+            double someNumber = 2.164403304907294849383961032d;
+
+            Console.WriteLine(number);
+            Console.WriteLine(partialNum);
+            Console.WriteLine(someNumber);
         }
     }
 }
